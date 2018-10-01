a7a91018a16ab9d6412fe337ba8da99d18c266d4
Elijah Balogun <elijahbalogun@Chibikes-MacBook-Pro.local>
2018-09-30 23:39:33 -0400

    changes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/surveyor'));
+app.use(express.static( '/dist/surveyor'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/surveyor/index.html'));
+res.sendFile(path.join('/dist/surveyor/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
